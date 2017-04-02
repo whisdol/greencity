fa6ef8b4cda83d229577d5dde6a25448371059e1
Cedric Lüke <=>
2017-04-02 11:28:23 +0200

    Add prototypical Spot Preview

--- GreenCityApp/src/pages/map/map.ts
+++ GreenCityApp/src/pages/map/map.ts
@@ -88,10 +88,10 @@ export class MapPage {
 	showMarkers() {
 		let spotMarker = 'assets/img/spotmarker.png';
 
-		let markers = [{'id': 12345, 'name': 'Escher Gärtchen', 'lat': 50.955639, 'lng': 6.948812},
-						{'id': 12346, 'name': 'Wickraths Strauß', 'lat': 50.954375, 'lng': 6.952084},
-						{'id': 12347, 'name': 'Kasparle', 'lat': 50.953486, 'lng': 6.953065},
-						{'id': 12348, 'name': 'Ringschen', 'lat': 50.949931, 'lng': 6.955110},];
+		let markers = [{'id': 12345, 'name': 'Escher Gärtchen', 'description': 'Das kleine Gärtchen an der Escherstr.', 'lat': 50.955639, 'lng': 6.948812},
+						{'id': 12346, 'name': 'Wickraths Strauß', 'description': 'Blümchen! Schöne Blumen zu jeder Jahreszeit.','lat': 50.954375, 'lng': 6.952084},
+						{'id': 12347, 'name': 'Kasparle', 'description': 'Gelbe Pflänzlein bevorzugt.','lat': 50.953486, 'lng': 6.953065},
+						{'id': 12348, 'name': 'Ringschen', 'description': 'Direkt am Hansaring: die kleine Oase.','lat': 50.949931, 'lng': 6.955110},];
 
 		markers.forEach((marker) => {
 			console.log(marker);
@@ -102,6 +102,26 @@ export class MapPage {
 				title: marker['name']
 			});
 			m.setMap(this.map);
+			var info = this.createInfoWindow(marker['name'], marker['description']);
+			m.addListener('click', function() {
+				info.open(this.map, m);
+			})
+		});
+	}
+
+	createInfoWindow(name: string, description: string) {
+		// Returns a Google Maps Info Window to associate with markers
+		var contentString = '<div id="content">'+
+		'<div id="siteNotice">'+
+		'</div>'+
+		'<h1 id="firstHeading" class="firstHeading">' + name + '</h1>'+
+		'<div id="bodyContent">'+
+		'<p>' + description + '</p>'+
+		'</div>'+
+		'</div>';
+
+		return new google.maps.InfoWindow({
+			content: contentString
 		});
 	}
 
